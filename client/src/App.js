4405870da94b74e6ec05aeca3ad96660b9645fda
AzureAD\OlkenMerxira <omerxira@unitsystem.net>
2025-06-08 17:32:34 +0200

    fixed profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import { useSelector } from "react-redux";
-import Spinner from "./components/Spinner";
-import ProtectedRoute from "./components/ProtectedRoute";
-import PublicRoute from "./components/PublicRoute";
-import ApplyDoctor from "./pages/ApplyDoctor";
-import NotificationPage from "./pages/NotificationPage";
-import Users from "./pages/admin/Users";
-import Doctors from "./pages/admin/Doctors";
-import Profile from "./pages/doctor/Profile";
-import BookingPage from "./pages/BookingPage";
-import Appointments from "./pages/Appointments";
-import DoctorAppointments from "./pages/doctor/DoctorAppointments";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { useSelector } from 'react-redux';
+import Spinner from './components/Spinner';
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
+import ApplyDoctor from './pages/ApplyDoctor';
+import NotificationPage from './pages/NotificationPage';
+import Users from './pages/admin/Users';
+import Doctors from './pages/admin/Doctors';
+import Profile from './pages/doctor/Profile';
+import BookingPage from './pages/BookingPage';
+import Appointments from './pages/Appointments';
+import DoctorAppointments from './pages/doctor/DoctorAppointments';
+import UserAdminProfile from './pages/UserAdminProfile';
+
 function App() {
-  const { loading } = useSelector((state) => state.alerts);
+  const { loading } = useSelector(state => state.alerts);
   return (
     <>
       <BrowserRouter>
@@ -23,6 +25,14 @@ function App() {
           <Spinner />
         ) : (
           <Routes>
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <UserAdminProfile />
+                </ProtectedRoute>
+              }
+            />
             <Route
               path="/apply-doctor"
               element={
---
+++ client/src/pages/UserAdminProfile.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const UserAdminProfile = () => {
+  return (
+    <Layout>
+      <div>
+        <p>Profile is not implemented yet.</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default UserAdminProfile;
